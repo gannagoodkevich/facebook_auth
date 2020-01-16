fe7fed866a230a62758b9f7150ebd3016180536b
gannagoodkevich <gannagoodkevich@gmail.com>
2020-01-16 15:28:12 +0300

    Add omniauths

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable,
+         omniauth_providers: [:facebook]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+
+  def apply_omniauth(auth)
+    update_attributes(
+        provider: auth.provider,
+        uid: auth.uid
+    )
+  end
+
+  def has_facebook_linked?
+    self.provider.present? && self.uid.present?
+  end
+end
